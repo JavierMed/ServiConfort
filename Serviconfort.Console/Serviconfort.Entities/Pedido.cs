4fd1dfaec81f8d1eac08442e942ce1645bbc91c4
Jorge Pedro Garcilazo Tume <Jorge Pedro Garcilazo Tume>
2017-05-11 13:37:28 -0500

    avance 3

--- Serviconfort.Console/Serviconfort.Entities/Pedido.cs
+++ Serviconfort.Console/Serviconfort.Entities/Pedido.cs
@@ -8,13 +8,54 @@ namespace Serviconfort.Entities
 {
     public class Pedido 
     {
-        private double codCompra { get; set; }
+        private int codCompra { get; set; }
         public string idProducto { get; set; }
         public string idProvedor { get; set; }
-        public int cantidad { get; set; }
-        private double precioCompra { get; set; }
-        private double descuento { get; set; }
+        public string cantidad { get; set; }
+        private string precioCompra { get; set; }
+        private string descuento { get; set; }
         public string idPago { get; set; }
-        public double codComprobante { get; set; }
+        public int codComprobante { get; set; }
+
+        public Pedido(int codcompra)
+        {
+            codCompra = codcompra;
+        }
+
+        public Pedido(string idproducto)
+        {
+            idProducto = idproducto;
+        }
+
+        public Pedido(string idprovedor)
+        {
+            idProvedor = idprovedor;
+        }
+
+        public Pedido(string Cantidad)
+        {
+            cantidad = Cantidad;
+        }
+
+        public Pedido(string preciocompra)
+        {
+            precioCompra = preciocompra;
+        }
+
+        public Pedido(string Descuento)
+        {
+            descuento = Descuento;
+        }
+
+        public Pedido(int idpago)
+        {
+            idPago = idpago;
+        }
+
+        public Pedido(int CodComprobante)
+        {
+            codComprobante = CodComprobante;
+        }
+
     }
 }
